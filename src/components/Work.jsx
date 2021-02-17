638ab6bb65c0a57739e78a6351f44854647c3e71
moses selete <55986824+aiden-riddler@users.noreply.github.com>
2021-02-17 11:25:05 +0300

    Add files via upload

---
+++ src/components/Work.jsx
@@ -0,0 +1,329 @@
+import React, {useState} from 'react';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import { withStyles } from '@material-ui/core/styles';
+import { green } from '@material-ui/core/colors';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Chip from '@material-ui/core/Chip';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import Professions from '../professions'
+
+const GreenCheckbox = withStyles({
+  root: {
+    color: green[400],
+    '&$checked': {
+      color: green[600],
+    },
+  },
+  checked: {},
+})((props) => <Checkbox color="default" {...props} />);
+
+
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    margin: '20px 50px',
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 20,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  chip: {
+    margin: '5px auto',
+  },
+});
+
+function Chores(){
+
+  const classes = useStyles();
+
+  const [chips, setChips] = React.useState({
+    selectedProffessions: [
+
+    ],
+  });
+
+  const handleClick = (event,value) => {
+    setChips((chips) => {
+      const selectedProffessions = [...chips.selectedProffessions,value];
+      return {
+        selectedProffessions
+      }
+    });
+    console.log(chips.selectedProffessions);
+
+  }
+
+  const handleDelete = (data) => {
+    setChips((chips) => {
+      const selectedProffessions = chips.selectedProffessions.filter((obj) => obj !== data);
+      return {
+        selectedProffessions
+      }
+    })
+  }
+
+  const [state, setState] = React.useState({
+    Filters:[{
+      head: 'Work',
+      mainQ: "Do you work?",
+      choicesQ: [{value:"Yes"},{value: "No"}],
+      q2: 'How should your roommate respond?'
+    }
+    ],
+    selected: [
+
+    ],
+  });
+
+
+  const handleChange = (id,value, mainQuestion) => {
+
+      setState((state) => {
+        const questionExists = state.selected.find((obj) => obj.mainQ === mainQuestion);
+
+        if (questionExists) {
+          console.log("question exists");
+          console.log(questionExists);
+          const qIndex = state.selected.findIndex((val) => val.mainQ === mainQuestion);
+          console.log(qIndex);
+          const updatedTable = state.selected[qIndex].answers.includes(value);
+          console.log(updatedTable);
+
+          const selected = updatedTable ?[...state.selected,{...questionExists,answers:questionExists.answers.filter((obj) => obj !== value)}]:
+          [{...state.selected.questionExists,answers:questionExists.answers.push(value)}];
+
+          return {
+            selected,
+            Filters: [
+                ...state.Filters.map((filter) => {
+                  return {
+                    ...filter,
+                    choicesQ: filter.choicesQ.map((ele) => {
+                      console.log(selected[qIndex].answers.includes(ele.value))
+                      return {
+                        ...ele,
+                        checked: selected[qIndex].answers.includes(ele.value)
+                      }
+                    })
+                  }
+                })
+              ]
+          }
+
+        }else{
+          console.log("question doesn't exists");
+          const obj = {
+            mainQ: mainQuestion,
+            answers : [value]
+          }
+          const selected = [...state.selected,obj];
+          return {
+            selected,
+            Filters: [
+                ...state.Filters.map((filter) => {
+                  return {
+                    ...filter,
+                    choicesQ: filter.choicesQ.map((ele) => {
+                      if (ele.value === value ){
+                        return {
+                          ...ele,
+                          checked: true
+                        }
+                      }else{
+                        return {
+                          ...ele,
+                          checked: false
+                        }
+                      }
+
+                    })
+                  }
+                })
+              ]
+          }
+        }
+      });
+          console.log(state.selected);
+  };
+
+  const [roomie, setRoomie] = React.useState({
+    Filters:[{
+      head: 'Work',
+      mainQ: "Do you work?",
+      choicesQ: [{value:"Yes"},{value: "No"}],
+      q2: 'How should your roommate respond?'
+    }
+    ],
+    selectedR: [
+
+    ]
+
+  });
+
+  const handleRoomieChange = (value, mainQuestion) => {
+    setRoomie((roomie) => {
+      const questionExists = roomie.selectedR.find((obj) => obj.mainQ === mainQuestion);
+
+      if (questionExists) {
+        console.log("question exists");
+        console.log(questionExists);
+        const qIndex = roomie.selectedR.findIndex((val) => val.mainQ === mainQuestion);
+        console.log(qIndex);
+        const updatedTable = roomie.selectedR[qIndex].answers.includes(value);
+        console.log(updatedTable);
+
+        const selectedR = updatedTable ?[...roomie.selectedR,{...questionExists,answers:questionExists.answers.filter((obj) => obj !== value)}]:
+        [{...roomie.selectedR.questionExists,answers:questionExists.answers.push(value)}];
+
+        return {
+          selectedR,
+          Filters: [
+              ...roomie.Filters.map((filter) => {
+                return {
+                  ...filter,
+                  choicesQ: filter.choicesQ.map((ele) => {
+                    console.log(selectedR[qIndex].answers.includes(ele.value))
+                    return {
+                      ...ele,
+                      checked: selectedR[qIndex].answers.includes(ele.value)
+                    }
+                  })
+                }
+              })
+            ]
+        }
+
+      }else{
+        console.log("question doesn't exists");
+        const obj = {
+          mainQ: mainQuestion,
+          answers : [value]
+        }
+        const selectedR = [...roomie.selectedR,obj];
+        return {
+          selectedR,
+          Filters: [
+              ...roomie.Filters.map((filter) => {
+                return {
+                  ...filter,
+                  choicesQ: filter.choicesQ.map((ele) => {
+                    if (ele.value === value ){
+                      return {
+                        ...ele,
+                        checked: true
+                      }
+                    }else{
+                      return {
+                        ...ele,
+                        checked: false
+                      }
+                    }
+
+                  })
+                }
+              })
+            ]
+        }
+      }
+    });
+    console.log(roomie.selectedR);
+  };
+  return(
+    <div>
+
+
+  <Card className={classes.root}>
+  <CardContent>
+  {state.Filters.map((ele) => {
+    return (
+      <React.Fragment>
+      <Typography className={classes.title} color="textSecondary" gutterBottom>
+        {ele.head}
+      </Typography>
+
+      <Typography variant="h5" component="h2">
+        {ele.mainQ}
+      </Typography>
+      {ele.choicesQ.map((item) => {
+
+        const {id,value} = item;
+        return (<FormGroup row>
+        <FormControlLabel
+         control={<GreenCheckbox onChange={() => handleChange(id,value,ele.mainQ)}/>}
+         label={item.value}/>
+        </FormGroup>)
+      })}
+      </React.Fragment>
+    )
+})}
+ <br />
+
+{roomie.Filters.map((ele) => {
+  return (
+    <React.Fragment>
+    <Typography className={classes.title} color="textSecondary" gutterBottom>
+      {ele.q2}
+    </Typography>
+
+    <Typography variant="h5" component="h2">
+      {ele.mainQ}
+    </Typography>
+    {ele.choicesQ.map((item) => {
+      return (<FormGroup row>
+      <FormControlLabel
+       control={<GreenCheckbox onChange={() => handleRoomieChange(item.value,ele.mainQ)}/>}
+       label={item.value}/>
+      </FormGroup>)
+    })}
+    </React.Fragment>
+  )
+})}
+<Typography className={classes.title} color="textSecondary" gutterBottom>
+  Which kind of professionals would you like to be matched with?
+</Typography>
+{chips.selectedProffessions.map((data) => {
+    return (
+      <li>
+            <Chip
+              label={data}
+              onDelete={() => handleDelete(data)}
+              className={classes.chip}
+            />
+          </li>
+    )
+  })}
+<Autocomplete
+    id="combo-box-demo"
+    options={Professions}
+    getOptionLabel={(option) => option}
+    style={{ width: 500 }}
+    onChange={handleClick}
+    renderInput={(params) => <TextField {...params} label="Professions" variant="outlined" />}
+  />
+
+  </CardContent>
+</Card>
+
+    </div>
+  )
+}
+
+export default Chores;
